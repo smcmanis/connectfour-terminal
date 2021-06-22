bb9bdff3a77186a37a676a22d1720c891c0c1cc2
smcmanis <simon.mcmanis@gmail.com>
2021-06-22 11:49:10 +1000

    Add console class

---
+++ console.py
@@ -0,0 +1,26 @@
+class bcolors:
+    YELLOW = "\033[93m"
+    RED = "\033[31m"
+    BLACK = "\033[0m"
+
+
+TOKENS = {-1: " ", 0: "R", 1: "Y"}
+TOKEN_COLORS = {-1: bcolors.BLACK, 0: bcolors.RED, 1: bcolors.YELLOW}
+
+
+def color_text(text, color):
+    return f"{color}{text}{bcolors.BLACK}"
+
+
+def draw_row(row):
+    colored_tokens = [color_text(TOKENS[token], TOKEN_COLORS[token]) for token in row]
+    return "|" + "|".join(colored_tokens) + "|"
+
+
+def draw_board(board):
+    rows = []
+    for y in range(board.height - 1, -1, -1):
+        row = [board.columns[x][y] for x in range(board.width)]
+        rows.append(draw_row(row))
+
+    print("\n".join(rows))
