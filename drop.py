7a8faa68d6f47494006e0085ff6c7b7e9e857c1d
smcmanis <simon.mcmanis@gmail.com>
2021-06-22 11:50:09 +1000

    Add drop class

---
+++ drop.py
@@ -0,0 +1,6 @@
+class Drop:
+    def __init__(self, player, board, column, row):
+        self.player = player
+        self.piece = player.piece
+        self.column = column
+        self.row = row
