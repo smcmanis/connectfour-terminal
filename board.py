fa09cbb0aa72b76b3eacb8eadc3ae9cf257fa661
smcmanis <simon.mcmanis@gmail.com>
2021-06-22 11:48:45 +1000

    Add board class

---
+++ board.py
@@ -0,0 +1,72 @@
+DEFAULT_HEIGHT = 6
+DEFAULT_WIDTH = 7
+PIECE_RED = 0
+PIECE_YELLOW = 1
+PIECE_NONE = -1
+
+
+class Board:
+    def __init__(self, height: int = DEFAULT_HEIGHT, width: int = DEFAULT_WIDTH):
+        self.columns = [[PIECE_NONE for y in range(height)] for x in range(width)]
+        self.height = height
+        self.width = width
+        self.last_drop = None
+
+    def pieces_in_column(self, x):
+        return self.height - self.columns[x].count(PIECE_NONE)
+
+    def pieces_in_board(self):
+        return sum(self.pieces_in_column(x) for x in range(self.width))
+
+    def drop_piece(self, x, piece: int = 0):
+        y = self.pieces_in_column(x)
+        self.columns[x][y] = piece
+        self.last_drop = (x, y)
+
+    def get_valid_moves(self):
+        return [x for x in range(self.width) if self.pieces_in_column(x) != self.height]
+
+    def is_out_of_bounds(self, x, y):
+        return x < 0 or x >= self.width or y < 0 or y >= self.height
+
+    def is_full(self):
+        for x in range(self.width):
+            if self.pieces_in_column(x) < self.height:
+                return False
+        return True
+
+    def last_drop_makes_connectfour(self):
+        if self.last_drop is None:
+            return False
+
+        x, y = self.last_drop
+        piece = self.columns[x][y]
+
+        # Look for vertical connected four (3 spots below)
+        if y - 3 >= 0 and self.columns[x][y - 3 : y].count(piece) == 3:
+            return True
+
+        # Look for horizontal (xxx[x] to [x]xxx)
+        for dx in range(4):
+            if self.is_out_of_bounds(x - dx, y) or self.is_out_of_bounds(x - dx + 4, y):
+                continue
+            cols = self.columns[x - dx : x - dx + 4]
+            count = [col[y] for col in cols].count(piece)
+            if count == 4:
+                return True
+
+        # Look for diagonal
+        for dx in range(3):
+            for gradient in [-1, 1]:
+                if self.is_out_of_bounds(
+                    x - dx, y - gradient * (dx)
+                ) or self.is_out_of_bounds(x - dx + 4, y - gradient * (dx + 4)):
+                    continue
+
+                diagonal = [
+                    self.columns[x - dx + i][y - gradient * (dx + i)] for i in range(3)
+                ]
+                if diagonal.count(piece) == 4:
+                    return True
+
+        return False
--- main.py
+++ main.py
@@ -1,28 +1,89 @@
-HUMAN = 0
-COMPUTER_EASY = 1
+import sys
+
+from player import Player
+from board import Board, PIECE_NONE, PIECE_RED, PIECE_YELLOW
+from console import draw_board
+
+
+class Game:
+    def __init__(self, board, players):
+        self.players = players
+        self.current_player = PIECE_RED
+        self.board = board
+
+    def get_move(self, player):
+        if player.is_computer():
+            move = player.automove(self.board.get_valid_moves())
+            self.board.drop_piece(move, self.current_player)
+            print(f"Computer dropped in column {move}")
+            return
+        else:
+            while True:
+                try:
+                    choice = int(
+                        input(
+                            f"==> Enter column to drop piece {self.board.get_valid_moves()}: "
+                        )
+                    )
+                    if choice in self.board.get_valid_moves():
+                        self.board.drop_piece(choice, self.current_player)
+                        return
+                    else:
+                        print("invalid selection! Try again")
+                except:
+                    print("invalid selection! Try again")
+
+    def start(self):
+        while True:
+
+            if self.board.is_full():
+                print("Game over: No player wins")
+                return
+
+            print()
+            draw_board(self.board)
+            print()
+
+            if self.board.last_drop_makes_connectfour():
+                self.current_player ^= 1
+                print(f"{self.players[self.current_player].color} wins!")
+                return
+
+            self.get_move(self.players[self.current_player])
+
+            self.current_player ^= 1
+
 
 def prompt_select_player(player=""):
-    options = [
-        ":: 0: Human",
-        ":: 1: Computer (easy)"
-    ]
-    print('\n'.join(options))
+    options = [":: 0: Human", ":: 1: Computer (easy)"]
+    print("\n".join(options))
     choice = input(f"==> Select player {player} type (default: 0): ")
+    if choice is "":
+        choice = 0
+
+    choice = int(choice)
     if choice == 0:
         print(f"Player {player} is Human")
-        return HUMAN
+        return choice
     else:
         print(f"Player {player} is Computer (easy)")
-        return COMPUTER_EASY
+        return choice
+
 
 def main():
     try:
-        playerRed = prompt_select_player("RED")
-        playerYellow = prompt_select_player("YELLOW")
+        while True:
+            players = []
+            players.append(Player(prompt_select_player("RED"), PIECE_RED))
+            players.append(Player(prompt_select_player("YELLOW"), PIECE_YELLOW))
+            board = Board()
+            Game(board, players).start()
+            restart = input("Play again? [y]").lower()
+            if restart != "y":
+                return
     except KeyboardInterrupt:
         sys.exit()
 
+
 if __name__ == "__main__":
     main()
-        
-
